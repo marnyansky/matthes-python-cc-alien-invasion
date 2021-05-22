908e7b63b1552de6ef591ae35e0e4e3b955eab74
Marnyansky <marnyansky@gmail.com>
2021-05-22 04:34:34 +0300

    spaceship speed set up, screen limits

--- settings.py
+++ settings.py
@@ -5,3 +5,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 0, 0)  # 82, 94, 84
+
+        """Spaceship settings"""
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -7,21 +7,28 @@ class Ship():
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         """Initial position of the spaceship"""
         self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
+        """Saving the real (вещественной) coordinate of the center of the spaceship"""
+        self.x = float(self.rect.x)
+
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Updates spaceship position depending on flag value (True/False)"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:  # not elif block: spaceship will always move to the right
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:  # attribute x is being updated, not rect ||| screen limits shouldn't be exceeded!
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:  # not elif block, because spaceship will always move to the right
+            self.x -= self.settings.ship_speed
+
+        # updating attribute rect based on self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draws a spaceship in a current position"""
